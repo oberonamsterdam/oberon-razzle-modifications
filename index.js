5ca485e5bc0b0249a9f6f196133c10f8b409e710
Jari Zwarts <jarizw@gmail.com>
2017-07-26 20:21:49 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = function(useEslint, usePolyfill) {
+    return function(config, options, webpack) {
+        // console.log(useEslint, usePolyfill, options, options.target);;
+        if(useEslint && options.dev) {
+            config.module.rules.unshift({
+                test: /\.(js|jsx)$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            });
+        }
+        
+        if(usePolyfill && options.target === 'web') {
+            // can't be bothered with polyfilling for the server; just use a modern version of node. 
+            config.entry.client.unshift('babel-polyfill'); 
+        }
+        
+        return config;
+    }
+}
